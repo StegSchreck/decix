dde5c1e778db9154b491e3c1bdfef0ec5e530969
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2018-01-02 19:15:24 +0100

    Remove User stuff for the beginning

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,22 +2,12 @@
   <div id="topnav">
     <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" router>
       <el-menu-item index="/"><img src="../assets/decix.png" height="50%"></el-menu-item>
-      <el-menu-item index="users">Users</el-menu-item>
       <el-menu-item index="apollo">Apollo</el-menu-item>
-      <el-container id="HeaderLoginButton">
-        <el-button type="primary" icon="el-icon-arrow-right" @click="goToLoginPage">Login</el-button>
-        <el-button icon="el-icon-arrow-right" @click="goToRegistrationPage">Register</el-button>
-      </el-container>
     </el-menu>
-    <!--<div class="line"></div>-->
   </div>
 </template>
 
 <style>
-  #HeaderLoginButton {
-    float: right;
-    margin: 10px;
-  }
 </style>
 
 <script>
@@ -29,12 +19,6 @@
       }
     },
     methods: {
-      goToLoginPage () {
-        this.$router.push('/login')
-      },
-      goToRegistrationPage () {
-        this.$router.push('/register')
-      }
     }
   }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Apollo from '@/components/Apollo'
-import Users from '@/components/Users'
-import Login from '@/components/Login'
-import Register from '@/components/Register'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/', name: 'Hello', component: HelloWorld },
-    { path: '/apollo', name: 'Apollo', component: Apollo },
-    { path: '/users', name: 'Users', component: Users },
-    { path: '/login', name: 'Login', component: Login },
-    { path: '/register', name: 'Register', component: Register }
+    { path: '/apollo', name: 'Apollo', component: Apollo }
   ]
 })
