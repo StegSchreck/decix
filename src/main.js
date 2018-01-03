409d54377c935f8d198eebca316553c1b69f19fd
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2018-01-03 15:49:26 +0100

    Remove Subscription stuff for now

--- src/main.js
+++ src/main.js
@@ -10,7 +10,6 @@ import '../theme/index.css'
 import 'isomorphic-fetch'
 
 import { ApolloClient, createNetworkInterface } from 'apollo-client'
-// import { SubscriptionClient, addGraphQLSubscriptions } from 'subscriptions-transport-ws'
 
 const GRAPHQL_SERVER_HOST = 'localhost'
 const GRAPHQL_SERVER_PORT = 3000
@@ -19,17 +18,7 @@ const networkInterface = createNetworkInterface({
   uri: `http://${GRAPHQL_SERVER_HOST}:${GRAPHQL_SERVER_PORT}/graphql`
 })
 
-// const wsClient = new SubscriptionClient(`ws://${GRAPHQL_SERVER_HOST}:${GRAPHQL_SERVER_PORT}/subscriptions`, {
-//   reconnect: true
-// })
-//
-// const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
-//   networkInterface,
-//   wsClient
-// )
-
 const apolloClient = new ApolloClient({
-  // networkInterface: networkInterfaceWithSubscriptions,
   networkInterface: networkInterface,
   connectToDevTools: true
 })
@@ -37,20 +26,12 @@ const apolloClient = new ApolloClient({
 Vue.use(VueApollo)
 Vue.use(ElementUI)
 
-// let loading = 0
-
 const apolloProvider = new VueApollo({
   clients: {
     a: apolloClient
   },
   defaultClient: apolloClient,
-  defaultOptions: {
-    // $loadingKey: 'loading',
-  },
-  // watchLoading (state, mod) {
-  //   loading += mod
-  //   console.log('Global loading', loading, mod)
-  // },
+  defaultOptions: {},
   errorHandler (error) {
     console.log('Global error handler')
     console.error(error)
