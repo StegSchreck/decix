1b6c7a6692ae375c709306ecf4cf00124a1c84fb
Sebastian Schreck <sebastian.schreck@stegschreck.de>
2018-01-12 22:30:28 +0100

    Add Buttons to implement later and refactored

--- src/components/Matrix.vue
+++ src/components/Matrix.vue
@@ -1,72 +1,76 @@
 <template>
   <div id="matrix">
-    <div class="section">
-      <h1>Matrix {{ $route.params.id }}</h1>
-      <div class="matrix" v-for="item in matrix">
-        <div :class="['matrix',{optimistic: item.id === -1}]" :title="item.id">
-          {{ item.id }} - {{ item.title }}<br>
-          {{ item.description }}
-        </div>
-        <el-button type="danger" plain icon="el-icon-delete" @click="deleteMatrix"/>
+    <div class="matrix" v-for="item in matrix">
+      <div class="section">
+        <h1>Matrix {{ $route.params.id }}</h1>
+          <div :class="['matrix',{optimistic: item.id === -1}]" :title="item.id">
+            {{ item.id }} - {{ item.title }}<br>
+            {{ item.description }}
+          </div>
+          <el-button-group>
+            <el-button type="primary" plain icon="el-icon-edit"/><!--@click="editMatrix"-->
+            <el-button type="info" plain icon="el-icon-download"/><!--@click="downloadMatrix"-->
+            <el-button type="danger" plain icon="el-icon-delete" @click="deleteMatrix"/>
+          </el-button-group>
       </div>
-    </div>
-    <div class="section">
-      <h1>Assigned categories</h1>
-      <el-button type="primary" icon="el-icon-circle-plus" plain @click="categoryDialogVisible = true" style="width: 100%"/>
-      <el-collapse v-if="matrix.categories">
-        <el-collapse-item v-for="item in matrix.categories" :key="item.id" :title="item.title" :name="item.id">
-          <div>Description: {{ item.description }}</div>
-          <div>Sorting: {{ item.sorting }}</div>
-          <div>Weight: {{ item.weight }}</div>
-        </el-collapse-item>
-      </el-collapse>
-      <el-alert
-        title="No category assigned yet."
-        type="info"
-        :closable="false"
-        center
-        show-icon
-        v-else/>
-    </div>
-    <div class="section">
-      <h2>Assigned alternatives</h2>
-      <el-button type="primary" icon="el-icon-circle-plus" plain @click="alternativeDialogVisible = true" style="width: 100%"/>
-      <el-collapse v-if="matrix.alternatives">
-        <el-collapse-item v-for="item in matrix.alternatives" :key="item.id" :title="item.title" :name="item.id">
-          <div>Description: {{ item.description }}</div>
-          <div>Sorting: {{ item.sorting }}</div>
-        </el-collapse-item>
-      </el-collapse>
-      <el-alert
-        title="No alternative assigned yet."
-        type="info"
-        :closable="false"
-        center
-        show-icon
-        v-else/>
-    </div>
+      <div class="section">
+        <h1>Assigned categories</h1>
+        <el-button type="primary" icon="el-icon-circle-plus" plain @click="categoryDialogVisible = true" style="width: 100%"/>
+        <el-collapse v-if="item.categories.length > 0">
+          <el-collapse-item v-for="category in item.categories" :key="category.id" :title="category.title" :name="category.id">
+            <div>Description: {{ category.description }}</div>
+            <div>Sorting: {{ category.sorting }}</div>
+            <div>Weight: {{ category.weight }}</div>
+          </el-collapse-item>
+        </el-collapse>
+        <el-alert
+          title="No category assigned yet."
+          type="info"
+          :closable="false"
+          center
+          show-icon
+          v-else/>
+      </div>
+      <div class="section">
+        <h2>Assigned alternatives</h2>
+        <el-button type="primary" icon="el-icon-circle-plus" plain @click="alternativeDialogVisible = true" style="width: 100%"/>
+        <el-collapse v-if="item.alternatives.length > 0">
+          <el-collapse-item v-for="alternative in item.alternatives" :key="alternative.id" :title="alternative.title" :name="alternative.id">
+            <div>Description: {{ alternative.description }}</div>
+            <div>Sorting: {{ alternative.sorting }}</div>
+          </el-collapse-item>
+        </el-collapse>
+        <el-alert
+          title="No alternative assigned yet."
+          type="info"
+          :closable="false"
+          center
+          show-icon
+          v-else/>
+      </div>
 
 
-    <el-dialog
-      title="Create new category"
-      :visible.sync="categoryDialogVisible"
-      width="30%">
-      <span>Here you can create a new category.</span>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="categoryDialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="categoryDialogVisible = false">Confirm</el-button>
-      </span>
-    </el-dialog>
-    <el-dialog
-      title="Create new alternative"
-      :visible.sync="alternativeDialogVisible"
-      width="30%">
-      <span>Here you can create a new alternative.</span>
-      <span slot="footer" class="dialog-footer">
-        <el-button @click="alternativeDialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="alternativeDialogVisible = false">Confirm</el-button>
-      </span>
-    </el-dialog>
+      <el-dialog
+        title="Create new category"
+        :visible.sync="categoryDialogVisible"
+        width="30%">
+        <span>Here you can create a new category.</span>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="categoryDialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="categoryDialogVisible = false">Confirm</el-button>
+        </span>
+      </el-dialog>
+      <el-dialog
+        title="Create new alternative"
+        :visible.sync="alternativeDialogVisible"
+        width="30%">
+        <span>Here you can create a new alternative.</span>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="alternativeDialogVisible = false">Cancel</el-button>
+          <el-button type="primary" @click="alternativeDialogVisible = false">Confirm</el-button>
+        </span>
+      </el-dialog>
+    </div>
   </div>
 </template>
 
